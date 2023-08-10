2b89b00167201a23243f36ecda25db762f81fc85
jorgeabrahan <jorge24abrahan@gmail.com>
2023-08-09 22:26:19 -0600

    Update validator import to use cdn link and add funcionality for copy button

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-import Validator from './Validator/Validator.js'
+import Validator from 'https://cdn.jsdelivr.net/gh/jorgeabrahan/ui_form_validation@d5e8b58/Validator/Validator.js'
 
 const form = document.getElementById('form')
 Validator.validate(form)
+
+/* Copy CDN link from copyInput */
+const copyInput = document.querySelector('.copy__input')
+const copyButton = document.querySelector('.copy__button')
+copyButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(copyInput?.value)
+  alert('Text copied!')
+})
